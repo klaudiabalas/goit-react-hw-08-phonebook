015de7b7da46591f941fb5ae06064f99481fdb5b
klaudiabalas <103740572+klaudiabalas@users.noreply.github.com>
2024-06-03 13:42:58 +0000

    Deploying to gh-pages from @ klaudiabalas/goit-react-hw-08-phonebook@ba1c8f31052a177c85ea4addd3842bde30e99c6f 🚀

---
+++ static/js/415.50d3bd85.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,o,n){n.r(o);var r=n(184);o.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Home page"}),(0,r.jsx)("p",{children:"Welcome to your own online phonebook! Now you can save phon numbers."})]})}}}]);
+//# sourceMappingURL=415.50d3bd85.chunk.js.map
