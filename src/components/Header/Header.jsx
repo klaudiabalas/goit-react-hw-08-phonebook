78c1a5d07433da079e10ee96f8dcf25a952447b8
klaudiab <klaudiabalas1303@gmail.com>
2024-06-03 15:58:07 +0200

    upd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from '../../hooks/useAuth';
 import { AuthNav } from './AuthNav/AuthNav';
+import css from './Header.module.css';
 import { Nav } from './Nav/Nav';
 import { UserMenu } from './UserMenu/UserMenu';
 
@@ -7,10 +8,17 @@ export const Header = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <header>
-      <h1>Phonebook</h1>
+    <header className={css.header}>
       <Nav />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      {isLoggedIn ? (
+        <div className={css.header_logged}>
+          <UserMenu />
+        </div>
+      ) : (
+        <div>
+          <AuthNav />
+        </div>
+      )}
     </header>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h2>Home page</h2>
       <p>
-        Welcome to your own online phonebook! Now you can save phon numbers.
+        Welcome to your own online phonebook! Now you can save phone numbers.
       </p>
     </div>
   );
